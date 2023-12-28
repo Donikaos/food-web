7f96ae52f00b2b1939322a82c897f6a11f219a74
Donikaos <104843751+Donikaos@users.noreply.github.com>
2023-12-28 16:17:54 +0000

    Added first Spring Boot Project

---
+++ hello/src/main/java/food/web/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package food.web.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloWorldController {
+ @RequestMapping(value="hello", method=RequestMethod.GET)
+  @ResponseBody
+  public String fetHelloWorld() {
+    return "<h1>Hello World Spring Boot !</h1";
+
+  }
+}
